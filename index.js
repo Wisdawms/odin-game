c8981742adc598cf75cfd229f5a1438beb1b17e7
wisdawms <medianhq@gmail.com>
2024-04-07 17:38:33 +0200

    ui done

--- index.js
+++ index.js
@@ -7,32 +7,40 @@ let playerScore = 0
     , computerScore = 0
     , currentRound = 0;
 const MAX_ROUNDS = 5;
+const MAX_SCORE = 5;
 const playForRounds = true;
+let ended = false;
+
+const yesBtn = document.querySelector("#yes");
+const noBtn = document.querySelector("#no");
+const playAgainDiv = document.querySelector('#play-again-div');
+
+const resultsText = document.querySelector('#results-text');
 
 function getComputerChoice() {
     // randomly return one of the choices "Rock, paper, scissors"
     return getRandomElement(choices)
 }
 
-function getPlayerChoice() {
-    let playerChoice = null;
-    let askPlayer = prompt("Rock[0], Paper[1] or Scissors[2]?: ");
-    if (playerChoice == null) {
-        if (/^\d+$/.test(askPlayer) && askPlayer in ['0', '1', '2']) {
-            playerChoice = choices[parseInt(askPlayer)];
-        }
-        else if (/^[A-Za-z]+$/.test(askPlayer) && choices.includes(askPlayer.toLowerCase())) {
-            playerChoice = askPlayer.toLowerCase();
-        }
-        else {
-            alert('Please enter a valid choice!');
-            getPlayerChoice();
-            return
-        }
-        console.log(`Player's choice: ${playerChoice}`);
-        return playerChoice
-    }
-}
+// function getPlayerChoice() {
+//     let playerChoice = null;
+//     let askPlayer = prompt("Rock[0], Paper[1] or Scissors[2]?: ");
+//     if (playerChoice == null) {
+//         if (/^\d+$/.test(askPlayer) && askPlayer in ['0', '1', '2']) {
+//             playerChoice = choices[parseInt(askPlayer)];
+//         }
+//         else if (/^[A-Za-z]+$/.test(askPlayer) && choices.includes(askPlayer.toLowerCase())) {
+//             playerChoice = askPlayer.toLowerCase();
+//         }
+//         else {
+//             alert('Please enter a valid choice!');
+//             getPlayerChoice();
+//             return
+//         }
+//         console.log(`Player's choice: ${playerChoice}`);
+//         return playerChoice
+//     }
+// }
 
 function playRound(playerChoice, computerChoice) {
     let roundMsg = '';
@@ -51,93 +59,140 @@ function playRound(playerChoice, computerChoice) {
         case 'rockrock':
         case 'paperpaper':
         case 'scissorsscissors':
-            alert(`${roundMsg} Tie!
+            //             alert(`${roundMsg} Tie!
+
+            // Player's score: ${playerScore}
+            // Computer's score: ${computerScore}`);
+
+            resultsText.textContent = `${roundMsg} Tie!
 
 Player's score: ${playerScore}
-Computer's score: ${computerScore}`);
+Computer's score: ${computerScore}`;
+
             break;
         case 'paperscissors':
         case 'scissorsrock':
         case 'rockpaper':
             playerScore++;
-            if (playerScore < 5 && computerScore < 5 || currentRound < MAX_ROUNDS) {
-                alert(`${roundMsg} Player wins this round!
+            if (playerScore < MAX_SCORE && computerScore < MAX_SCORE || currentRound < MAX_ROUNDS) {
+                //                 alert(`${roundMsg} Player wins this round!
+
+                // Player's score: ${playerScore}
+                // Computer's score: ${computerScore}`);
+
+                resultsText.textContent = `${roundMsg} Player wins this round!
 
 Player's score: ${playerScore}
-Computer's score: ${computerScore}`);
+Computer's score: ${computerScore}`;
             }
             break;
         case 'scissorspaper':
         case 'rockscissors':
         case 'paperrock':
             computerScore++;
-            if (playerScore < 5 && computerScore < 5 || currentRound < MAX_ROUNDS) {
-                alert(`${roundMsg} Computer wins this round!
+            if (playerScore < MAX_SCORE && computerScore < MAX_SCORE || currentRound < MAX_ROUNDS) {
+                //                 alert(`${roundMsg} Computer wins this round!
+
+                // Player's score: ${playerScore}
+                // Computer's score: ${computerScore}`);
+
+                resultsText.textContent = `${roundMsg} Computer wins this round!
                 
 Player's score: ${playerScore}
-Computer's score: ${computerScore}`);
+Computer's score: ${computerScore}`;
             }
             break;
     }
+
+    if ((!playForRounds && playerScore >= MAX_SCORE) || (playForRounds && playerScore > computerScore && currentRound >= MAX_ROUNDS)) {
+        //     alert(`Player wins the game!
+
+        // Player's score: ${playerScore}
+        // Computer's score: ${computerScore}`);
+
+        resultsText.textContent =
+            `Player wins the game!
+
+Player's score: ${playerScore}
+Computer's score: ${computerScore}`;
+        ended = true;
+    }
+    else if ((!playForRounds && computerScore >= MAX_SCORE) || (playForRounds && computerScore > playerScore && currentRound >= MAX_ROUNDS)) {
+        //     alert(`Computer wins the game!
+
+        // Player's score: ${playerScore}
+        // Computer's score: ${computerScore}`);
+
+        resultsText.textContent = `Computer wins the game!
+    
+Player's score: ${playerScore}
+Computer's score: ${computerScore}`;
+        ended = true;
+    }
+    else if (playForRounds && currentRound >= MAX_ROUNDS && computerScore == playerScore) {
+        //     alert(`The game is a TIE!
+
+        // Player's score: ${playerScore}
+        // Computer's score: ${computerScore}`);
+
+        resultsText.textContent = `The game is a TIE!
+
+Player's score: ${playerScore}
+Computer's score: ${computerScore}`;
+        ended = true;
+    }
+
+    if (ended) {
+        playAgainDiv.style = "display: flex;"
+        yesBtn.onclick = () => {
+            ended = false;
+            currentRound = 1;
+            playerScore = 0;
+            computerScore = 0;
+            resultsText.textContent = 'New game started';
+            playAgainDiv.style = "display: none;"
+            return
+        }
+
+        noBtn.onclick = () => {
+            playAgainDiv.style = "display: none;"
+            resultsText.textContent = "";
+            return
+        }
+    }
 }
 
+
+const choiceButtons = document.querySelectorAll('.choice-btn');
 
 function playGame() {
     playerScore = 0;
     computerScore = 0;
     currentRound = 1;
-    if (playForRounds) {
-        while (currentRound < MAX_ROUNDS + 1) {
-            playRound(getPlayerChoice(), getComputerChoice());
-            currentRound++;
-        }
-    }
-    else {
-        while (playerScore < 5 && computerScore < 5) {
-            playRound(getPlayerChoice(), getComputerChoice())
-        }
 
-    }
-    console.log('amogus');
-    console.log(playerScore, computerScore);
-    if (playerScore > computerScore) {
-        alert(`Player wins the game!
+    choiceButtons.forEach((btn) => {
+        btn.addEventListener('click', (e) => {
+            if (playForRounds) {
+                if (currentRound < MAX_ROUNDS + 1) { // if haven't ended
+                    playRound(btn.id, getComputerChoice())
+                    currentRound++;
+                }
+            }
 
-Player's score: ${playerScore}
-Computer's score: ${computerScore}`);
-    }
-    else if (computerScore > playerScore) {
-        alert(`Computer wins the game!
+            else {
+                if (playerScore < MAX_SCORE + 1 && computerScore < MAX_SCORE + 1) { // if haven't won yet
+                    playRound(btn.id, getComputerChoice())
+                    currentRound++;
+                }
 
-Player's score: ${playerScore}
-Computer's score: ${computerScore}`);
-    }
-    else if (computerScore == playerScore) {
-        alert(`The game is a TIE!
+            }
 
-Player's score: ${playerScore}
-Computer's score: ${computerScore}`);
-    }
-
-    let again = null;
-
-    while (!['yes', 'no'].includes(again)) {
-        again = prompt('Play again? (yes/no): ').toLowerCase();
-        switch (again) {
-            case 'yes':
-            case 'yeah':
-            case 'yeah':
-            case 'ye':
-            case 'yay':
-                playGame();
-                break;
-            case 'no':
-            case 'nah':
-            case 'nay':
-            case 'naah':
-                return
-        }
-    }
+        })
+    });
 }
 
 playGame();
+
+
+
+
